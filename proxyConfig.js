57214a0c0a6252343ef7e5f64b9e2e68779a7014
tuyang <coocld@sina.com>
2017-03-07 17:25:05 +0800

    -

---
+++ proxyConfig.js
@@ -0,0 +1,11 @@
+'use strict';
+//devProxy config
+module.exports = {
+    remote: 'https://xxoo.github.io/',
+    local: {
+        prefix: '/hao/',
+        root: './',
+        index: 'index.html'
+    },
+    port: 2000
+};
