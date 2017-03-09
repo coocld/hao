a59b0e0e41f1c9ec84bf981b1a2d4534a08635a7
tuyang <coocld@sina.com>
2017-03-09 14:19:19 +0800

    -

--- dev/page/home/home.js
+++ dev/page/home/home.js
@@ -2,11 +2,11 @@
 define(['module', 'common/kernel/kernel', 'common/vue/vue'], function(module, kernel, Vue) {
 	var thisPage = module.id.replace(/^[^/]+\/|\/[^/]+/g, ''),
 		dom = $('#' + thisPage);
-	var i = 0;
-	dom.find('>a').on('click', function() {
-		i++;
-		kernel.openPanel('samplePanel', i);
-	});
+	// var i = 0;
+	// dom.find('>a').on('click', function() {
+	// 	i++;
+	// 	kernel.openPanel('samplePanel', i);
+	// });
 
 	var app6 = new Vue({
 	  el: '#app-6',
